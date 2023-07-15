f90cb6af716a9b537e70f97bb0fb0d144290514f
giulianac-git <79769030+giulianac-git@users.noreply.github.com>
2023-07-15 04:29:07 +0000

    Final Version: API linked and prompt for empty value added

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,20 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import home from "./home.jsx";
 
 function List() {
     const [newTodo, setNewTodo] = useState("");
     const [todos, setTodos] = useState([]);
-  
+
+    // GET API Tools - Loads the todos list on mount.
+    useEffect(() => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/giulianac", {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setTodos(data);
+            })
+            .catch((error) => {
+                console.log("Error", error);
+            });
+    }, []);
+
+    //Renders when the todos are updated.
+    useEffect(() => { updateDatabase() }, [todos]);
+
+    //PUT API Tool - To add or delete a todo.
+    const updateDatabase = () => {
+        fetch("https://assets.breatheco.de/apis/fake/todos/user/giulianac", {
+            method: "PUT",
+            body: JSON.stringify(todos),
+            headers: { "Content-Type": "application/json" },
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log("Error", error);
+            });
+    };
+
+    const clearList = () => {
+        // Function for the button to clear the todo list.
+        setTodos([]);
+        updateDatabase([]);
+    };
+
     return (
         <>
-            <ul>
+            <ul className="todos-list">
                 <li className="input-task">
                     <input type="text" placeholder="Add A New Task"
                         onChange={e => setNewTodo(e.target.value)}
                         value={newTodo}
                         onKeyDown={(e) => {
-                            if (e.key === "Enter") {
-                                setTodos(todos.concat(newTodo));
+                            if (e.key === "Enter" && e.target.value === "") {
+                                alert("You need to add a task first!")
+                            }
+                            else if (e.key === "Enter") {
+                                setTodos(todos.concat({ label: newTodo, done: false }));
                                 setNewTodo("");
+                                updateDatabase();
                             }
                         }}
                     ></input>
@@ -23,8 +72,12 @@ function List() {
                 {todos.map((item, index) => {
                     return (
                         <li key={index}>
-                            {item} <i class="btn-close" 
-                            onClick={() => setTodos(todos.filter((t, currentIndex) => index !== currentIndex))}/>
+                            {item.label}
+                            <i class="btn-close"
+                                onClick={() => {
+                                    setTodos(todos.filter((t, currentIndex) => index !== currentIndex));
+                                    updateDatabase();
+                                }} />
                         </li>
                     )
                 })}
@@ -37,3 +90,4 @@ function List() {
 }
 
 export default List;
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import List from "./List";
+import Proptypes from "prop-types";
 
 //Main Component
-const Home = () => {
+const Home = (props) => {
 	return (
 		<div className="container mt-5">
-			<h1 className="header text-center">To-Dos</h1>
+			<h1 className="header text-center">To-dos List</h1>
 			<div className="card">
 				<div className="list">
 					<List />
 				</div>
 			</div>
+			<button className="btn btn-danger mt-5"
+					onClick={props.clearList}>
+					Delete All
+			</button>
 		</div>
 	);
+};
+
+Home.propTypes = {
+	clearList: Proptypes.func,
 };
 
 export default Home;
